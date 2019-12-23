11f24d2f82345ff86af192c9049f517d3549478f
andrew <andrewpcs2016@gmail.com>
2019-12-23 10:46:50 +0500

    added additional imports

--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -1,35 +1,61 @@
-create schema public
-;
-
-comment on schema public is 'standard public schema'
+create sequence users_id_seq
 ;
 
 create table applications
 (
-	id integer not null
+	id serial not null
 		constraint applications_id_pk
 			primary key,
 	title varchar(50) not null,
-	text varchar(50)
+	text varchar(50) not null,
+	date_create timestamp not null
 )
 ;
 
 create table users
 (
-	id serial not null
-		constraint users_id_pk
-			primary key,
+	id serial not null,
 	first_name varchar(50) not null,
 	last_name varchar(50) not null,
 	password varchar(100) not null,
 	address varchar(50),
-	date_birth time not null,
-	username varchar(50) not null
+	username varchar(50) not null,
+	date_birth date
 )
 ;
 
-create unique index users_login_uindex
+create unique index users_username_uindex
 	on users (username)
 ;
 
-create sequence users_id_seq;
+create table comments
+(
+	id serial not null
+		constraint comments_pkey
+			primary key,
+	text varchar(50) not null
+)
+;
+
+create table applications_comments
+(
+	application_id integer not null,
+	comment_id integer not null
+)
+;
+
+create table applications_users
+(
+	application_id integer not null,
+	user_id integer not null
+)
+;
+
+-- auto-generated definition
+-- No source text available
+create SEQUENCE applications_id_seq;
+-- auto-generated definition
+CREATE SEQUENCE users_id_seq;
+-- auto-generated definition
+-- No source text available
+create sequence comments_id_seq;
